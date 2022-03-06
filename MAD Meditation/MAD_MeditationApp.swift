cae95657b41a52f67e0dff053722cd73987fabdd
delet-dis <efimov.036@gmail.com>
2022-03-06 12:54:47 +0700

    ➕added basic project files

---
+++ MAD Meditation/MAD_MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD_MeditationApp.swift
+//  MAD Meditation
+//
+//  Created by Igor Efimov on 06.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MAD_MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
