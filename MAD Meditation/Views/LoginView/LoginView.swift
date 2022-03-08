d4ff1bb241725bbcb53e888d67adaaf3b1d1d60a
delet-dis <efimov.036@gmail.com>
2022-03-08 15:18:52 +0700

    ☑️done login screen layout building

--- MAD Meditation/Views/LoginView/LoginView.swift
+++ MAD Meditation/Views/LoginView/LoginView.swift
@@ -26,6 +26,58 @@ struct LoginView: View {
                 .padding(.leading, 35)
             
             TextField(NSLocalizedString("Email", comment: ""), text: $viewModel.enteredEmailText)
+                .overlay {
+                    VStack {
+                        Divider()
+                            .offset(x: 0, y: 20)
+                    }
+                }
+                .foregroundColor(.white)
+                .padding(.leading, 35)
+                .padding(.trailing, 35)
+            
+            SecureField(NSLocalizedString("Password", comment: ""), text: $viewModel.enteredPasswordText)
+                .overlay {
+                    VStack {
+                        Divider()
+                            .offset(x: 0, y: 20)
+                    }
+                }
+                .foregroundColor(.white)
+                .padding(.leading, 35)
+                .padding(.trailing, 35)
+                .padding(.top, 35)
+            
+            HStack(alignment: .center) {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(minWidth: 0, idealWidth: 100, maxWidth: 600, minHeight: 60, idealHeight: 60, maxHeight: 60, alignment: .center)
+                            .foregroundColor(Colors.secondaryColor)
+                            .padding(.leading, 25)
+                            .padding(.trailing, 25)
+                            
+                        
+                        Text(NSLocalizedString("SignIn", comment: ""))
+                            .foregroundColor(.white)
+                            .font(.custom("AlegreyaSans-Medium", size: 25))
+                    }
+                }
+                .padding(.top, 55)
+                Spacer()
+            }
+            .ignoresSafeArea()
+            
+            NavigationLink(destination: RegisterView()) {
+                Text(NSLocalizedString("Register", comment: ""))
+                    .foregroundColor(.white)
+                    .font(.custom("AlegreyaSans-Regular", size: 20))
+            }
+            .padding(.top, 23)
+            .padding(.leading, 35)
             
             Spacer()
         }
@@ -39,5 +91,6 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+            .environmentObject(LoginViewModel())
     }
 }
